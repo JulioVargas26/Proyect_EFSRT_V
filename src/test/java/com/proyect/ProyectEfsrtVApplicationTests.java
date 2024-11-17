ae976a302bd9071b856a9d1088e45ec6ead556ae
JulioVargas26 <juliocvl2614@gmail.com>
2024-11-16 23:14:22 -0500

    commit

--- src/test/java/com/proyect/ProyectEfsrtVApplicationTests.java
+++ src/test/java/com/proyect/ProyectEfsrtVApplicationTests.java
@@ -2,19 +2,19 @@ package com.proyect;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.boot.test.context.SpringBootTest;/*
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;*/
 
 @SpringBootTest
 class ProyectEfsrtVApplicationTests {
 
-    @Autowired
+   /* @Autowired
     private BCryptPasswordEncoder encoder;
-
+*/
     @Test
     void contextLoads() {
-        String password = encoder.encode("admin");
-        System.out.println("Clave es : " + password);
+      /*  String password = encoder.encode("admin");
+        System.out.println("Clave es : " + password);*/
     }
 
 }
